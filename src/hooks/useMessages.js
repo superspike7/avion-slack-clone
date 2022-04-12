1d83077f701c1abfcc74fe33b45cd3456ee3b2b9
Spike Vinz <spikevinz@gmail.com>
2022-04-12 22:02:47 +0800

    adds real-time message fetching

--- src/hooks/useMessages.js
+++ src/hooks/useMessages.js
@@ -3,8 +3,13 @@ import { useQuery } from "react-query";
 import * as API from "../api";
 
 export default function useMessages(id, type) {
-  const { data } = useQuery(["messages", `${id}`], () =>
-    API.fetchMessages(id, type)
+  const { data } = useQuery(
+    ["messages", `${id}`],
+    () => API.fetchMessages(id, type),
+    {
+      // Refetch the data every second
+      refetchInterval: 1000,
+    }
   );
   const [messages, setMessages] = useState([]);
 
