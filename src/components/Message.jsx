1f2af4d3178d29a42e678a49812d0495278ab814
Spike Vinz <spikevinz@gmail.com>
2022-04-13 00:12:53 +0800

    adds avatars

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -8,10 +8,14 @@ export default function Message({ message }) {
     });
   };
 
+  const AVATAR = "https://avatars.dicebear.com/api/micah/";
+
   return (
     <div className="hover:bg-gray-300 py-2">
       <div className="flex px-5 gap-2">
-        <div className="bg-blue-500 h-10 w-10"></div>
+        <div className="h-10 w-10 bg-primary bg-opacity-20 rounded-md">
+          <img src={`${AVATAR}avion-${message.sender.email}.svg`} />
+        </div>
         <div className="flex flex-col flex-1">
           <div className="flex gap-2 items-end">
             <p>{message.sender.email}</p>
