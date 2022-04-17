146e173afdc5d449cfab023efffd706b0d97b0d1
Spike Vinz <spikevinz@gmail.com>
2022-04-17 09:21:28 +0800

    adds Dashboard content

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+import MessageForm from "./MessageForm";
+
 export default function Dashboard() {
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h1 className="text-center my-2 text-xl">Start a Conversation</h1>
+      <MessageForm />
+      <h1 className="text-center my-2 text-xl">Or Create a Channel</h1>
+      <Link
+        to="channels/new"
+        className="bg-primary text-gray-50 rounded-md p-2 block mx-auto max-w-max"
+      >
+        Create New Channel
+      </Link>
     </div>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,10 +6,13 @@ export default function Nav() {
 
   return (
     <div className="bg-primary p-2">
-      <h1 className="text-center text-gray-50 text-xl font-semibold mb-8
-                     border-y-[0.1px] border-y-fuchsia-200 py-2 border-opacity-10">
+      <Link
+        to="/"
+        className="text-center text-gray-50 text-xl font-semibold mb-8 block
+                   border-y-[0.1px] border-y-fuchsia-200 py-2 border-opacity-10"
+      >
         Avion School
-      </h1>
+      </Link>
       <div className="px-4 text-gray-50 mb-8">
         <div className="flex justify-between">
           <h3>Channels</h3>
@@ -37,7 +40,7 @@ export default function Nav() {
       </div>
       <div className="px-4 text-gray-50">
         <div className="flex justify-between">
-          <h3>Messages</h3>
+          <h3>Direct Messages</h3>
           <Link
             to="/messages/new"
             className="hover:bg-gray-100 hover:text-gray-900 px-2 rounded-full"
@@ -45,14 +48,6 @@ export default function Nav() {
             &#x1F50D;
           </Link>
         </div>
-        <ul className="w-10/12 mx-auto">
-          <Link
-            to="/dm/69"
-            className="hover:bg-gray-800 cursor-pointer w-full block"
-          >
-            Vinz
-          </Link>
-        </ul>
       </div>
     </div>
   );
