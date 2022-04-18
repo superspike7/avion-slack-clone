2dd6a6be2a4946429fbb42bfda6e9dfe150a482f
Spike Vinz <spikevinz@gmail.com>
2022-04-18 19:58:04 +0800

    removes unused hook

--- src/hooks/useDirectMessages.js
+++
@@ -1,7 +0,0 @@
-import useUsers from "./useUsers";
-import * as API from "../api";
-import { useQuery } from "react-query";
-
-export default function useDirectMessages() {
-  const { users } = useUsers();
-}
